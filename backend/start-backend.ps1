9c892187353daa0d66228a1efa1244e3538c7427
Makino2002 <Maiphuc455@gmail.com>
2025-06-09 19:35:44 +0700

    khoc ca mua xuan

--- backend/start-backend.ps1
+++ backend/start-backend.ps1
@@ -1,11 +1,10 @@
-# stop old process if needed
+
 $process = Get-Process node -ErrorAction SilentlyContinue
 if ($process) {
     Stop-Process -Name node -Force
     Write-Host "Đã dừng tiến trình Node cũ"
 }
 
-# start new one in background with logging
 Start-Process `
   -FilePath "C:\Program Files\nodejs\node.exe" `
   -ArgumentList "F:\todoapp-with-dom\backend\server.js" `

6e35c5c90554674d9a5d64ea95a18e10ec19ea2f
Makino2002 <Maiphuc455@gmail.com>
2025-06-09 19:00:39 +0700

    abc

---
+++ backend/start-backend.ps1
@@ -0,0 +1,15 @@
+# stop old process if needed
+$process = Get-Process node -ErrorAction SilentlyContinue
+if ($process) {
+    Stop-Process -Name node -Force
+    Write-Host "Đã dừng tiến trình Node cũ"
+}
+
+# start new one in background with logging
+Start-Process `
+  -FilePath "C:\Program Files\nodejs\node.exe" `
+  -ArgumentList "F:\todoapp-with-dom\backend\server.js" `
+  -RedirectStandardOutput "F:\todoapp-with-dom\backend\logs\stdout.log" `
+  -RedirectStandardError "F:\todoapp-with-dom\backend\logs\stderr.log" `
+  -NoNewWindow
+Write-Host "🎉 Backend khởi động"
